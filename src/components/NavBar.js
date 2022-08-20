3c7c3c3e74c8a8264281a731bf1fde6e809705eb
Sevenpros <sethbisoso@gmail.com>
2022-08-20 11:45:06 +0200

    Add styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   const links = [
+
     {
       id: 1,
       path: '/',
-      text: 'Books',
+      text: 'BOOKS',
     },
     {
       id: 2,
       path: '/categories',
-      text: 'Categories',
+      text: 'CATEGORIES',
     },
   ];
   return (
     <nav>
       <ul className="navBar">
-        {links.map((link) => (
-          <li key={link.id}>
-            <Link to={link.path} activeclassname="active-link">
-              {link.text}
-            </Link>
+        <div className="navList">
+          <li id="logo">
+            <Link to="/">Bookstore CMS </Link>
           </li>
-        ))}
+          {links.map((link) => (
+            <li key={link.id}>
+              <Link to={link.path} activeclassname="active-link">
+                {link.text}
+              </Link>
+            </li>
+          ))}
+        </div>
+        <div className="profile-icon">
+          <FontAwesomeIcon icon={faUserCircle} />
+        </div>
       </ul>
     </nav>
   );
